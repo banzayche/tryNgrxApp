ba9eb4b69f56b70afb91e4f9da07cfea98d69f5d
banzayche <dharm.serg@gmail.com>
2018-08-17 13:19:44 +0300

    Add gulp command to fix paths for ghp. Add before deploy task.

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var dom = require('gulp-dom');
+const ghpPathPref = './';
+
+function fixPathProcedure(arr, attrToFix, pref) {
+  for (let index = 0; index < arr.length; index++) {
+    const tag = arr[index];
+    let _attrToFix = `${pref}${tag[attrToFix]}`;
+    tag[attrToFix] = _attrToFix;
+  }
+}
+
+gulp.task('fix.ghp.paths', function () {
+  return gulp.src('./dist/tryNgrxStore/index.html')
+    .pipe(dom(function () {
+      const scripts = {
+        nodes: this.querySelectorAll('script'),
+        attrToChange: 'src'
+      };
+      const links = {
+        nodes: this.querySelectorAll('link'),
+        attrToChange: 'href'
+      };
+
+      fixPathProcedure(scripts.nodes, scripts.attrToChange, ghpPathPref);
+      fixPathProcedure(links.nodes, links.attrToChange, ghpPathPref);
+
+      return this;
+    }))
+    .pipe(gulp.dest('./dist/tryNgrxStore'));
+});
